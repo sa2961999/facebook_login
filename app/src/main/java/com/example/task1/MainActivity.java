b5e84c8f12452414907a8c2ec983c1c8e9c3d282
sa2961999 <samarbasiouni55@gmail.com>
2021-07-16 00:01:37 +0200

    hello world

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this,"on create",Toast.LENGTH_SHORT).show();
+        Log.d("test_on create","on create");
         setContentView(R.layout.activity_main);
         password=findViewById(R.id.et2);
         username=findViewById(R.id.et1);
@@ -42,5 +45,40 @@ public class MainActivity extends AppCompatActivity {
               }
 
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("test_on start","on start");
+        Toast.makeText(this,"on start",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("test_on resume","on resume");
+        Toast.makeText(this,"on resume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("test_on pause","on pause");
+        Toast.makeText(this,"on pause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("test_on stop","on stop");
+        Toast.makeText(this,"on stop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("test_on destroy","on destroy");
+        Toast.makeText(this,"on destroy",Toast.LENGTH_SHORT).show();
     }
 }
